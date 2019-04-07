4636cf529789596e1c1d7817d6a4f1911df8a2d0
Anthony Wan <anthonywan00@gmail.com>
2019-04-07 20:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var height = "74"
 if (name === "Susan")  {
   console.log("The name is correct")
 }
+else  {
+  console.log("Expected name to be Susan")
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
